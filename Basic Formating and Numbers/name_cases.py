8e521561e4d30916abb580937e9dcfc4c7dc21bc
Akshit Gupta <akshitgupta29@gmail.com>
2019-12-23 17:38:11 +0530

    Basic Numbers and formatting

---
+++ Basic Formating and Numbers/name_cases.py
@@ -0,0 +1,30 @@
+# Python Crash Course
+# Try it yourself 
+# author:akshitgupta29
+
+# Prob 2.3
+name = 'Akshit'
+print (f"Hello {name}, would you like to learn some Python today?")
+
+# prob 2.4
+name = 'akshit'
+print (name)
+print (name.lower())
+print (name.upper())
+print (name.title())
+
+#prob 2.5
+print ('Albert Einstein once said, “A person who never made a mistake never tried anything new.”')
+
+#prob 2.6
+famous_person = 'Albert Einstein'
+message = f'{famous_person} once said, “A person who never made a mistake never tried anything new.”'
+print (message)
+
+#prob 2.7
+name = ' \t Akshit \n   Gupta  '
+print (name)
+print (name.lstrip())
+print (name.rstrip())
+print (name.strip())
+
