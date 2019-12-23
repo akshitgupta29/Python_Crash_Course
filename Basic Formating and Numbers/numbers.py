80779344160c3d2cf1d015b2af919d3e7bd13145
Akshit Gupta <akshitgupta29@gmail.com>
2019-12-23 17:52:09 +0530

    Basics finished

---
+++ Basic Formating and Numbers/numbers.py
@@ -0,0 +1,23 @@
+# Self learning on the basis of the numbers chapter
+
+# no effect of the _ in number
+my_number = 1_40_000
+print (my_number)
+
+#multiple assignments
+x, y, z = 0,0,0
+print (x, y, z)
+
+#if we try to pack more than the expected values, we get a value error with the message as "ValueError: too many values to unpack (expected 3)"
+#x, y, z = 0,0,0, 0
+#print (x, y, z)
+
+#exponentials **
+print (2**3)
+
+# variable names with the capitals are called as constants and their value is not expected to change throughout the program.
+MY_AKSHIT = 100
+
+
+
+
