c6e7d30a92668043babce9bb391fb584727fcade
Akshit Gupta <akshitgupta29@gmail.com>
2019-12-24 13:09:15 +0530

    lists in action

---
+++ Lists/lists.py
@@ -0,0 +1,18 @@
+# Self learning for the lists
+#author: akshitgupta29
+
+#Displays all the elements including the square bracket
+bicycles = ['Akshit', 'Abhinav', 'Manju', 'Pardeep']
+print (bicycles)
+
+#Only first element without square bracket
+print(bicycles[0])
+
+#For accessing the last element, we use -1 as the index.
+print (bicycles[-1])
+
+## Adding the elements to the list
+# 1. Appending at the last of the list: The simplest way to add a new element to a list is to append the item to the list. When you append an item to a list, the new element is added to the end of the list.
+
+bicycles.append('Ashutosh')
+print(bicycles)
---
+++ Lists/try_it_yourself.py
@@ -0,0 +1,13 @@
+#author: akshitgupta29
+
+#Soltuions to the try it yourself problems in the book
+#NOTE: Some of the exercises done on my previous knowledge of the for loops.
+
+#3.1 Names
+names = ['Akshit', 'Abhinav', 'Ashutosh', 'Abhishek']
+for name in names:
+    print(name)
+
+#3.2 
+for name in names:
+    print(f"Hello {name}")
