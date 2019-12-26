b782051aa2d59e84ca1e7356d5f6642ecb744d4f
Akshit Gupta <akshitgupta29@gmail.com>
2019-12-26 23:13:06 +0530

    starting the iif loop

---
+++ IF Statements/IF_statements.py
@@ -0,0 +1,11 @@
+# Self learning for the if else.
+#author: akshitgupta29
+
+# Testing for equality is case sensitive in Python.
+car = 'audi'
+print (car == 'Audi') #False as the vales are considered differents.
+# to avoid this, convert all the user input to any case of your choice and then do the comparison.
+
+# Checking Multiple Conditions
+# The keywords 'and' and 'or' can help you in these situations.
+
